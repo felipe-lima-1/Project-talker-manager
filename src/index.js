6c538cd582b0ae9c30427272a742049dd8de7a53
Felipe <luizbrutante@gmail.com>
2022-12-14 15:35:28 -0300

    Requisito 1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 app.use(express.json());
 
+app.get('/talker', async (req, res) => {
+const data = JSON.parse(fs.readFileSync('src/talker.json', 'utf-8'));
+  res.status(200).json(data);
+});
+
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
-
 app.listen(PORT, () => {
   console.log('Online');
 });
